f9f0fb86745efb850331fb011cdaf7b44db2a033
Josue <mexic@DESKTOP-8QCBTRG>
2023-02-12 13:56:33 -0600

    Cambios esteticos del GUI

--- GUI/frmInventario.Designer.cs
+++ GUI/frmInventario.Designer.cs
@@ -240,6 +240,7 @@
             this.button1.TabIndex = 10;
             this.button1.Text = "Cancelar";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // frmInventario
             // 
